f3d555d0b0753be570eab4101e52b928bbea53d9
FiftyBillion <pianoboy1103@gmail.com>
2020-02-11 01:28:34 -0800

    update and applying vuex

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,28 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    PBitems: [
+      { content: "Hello 1", priority: "High", hour: "10" },
+      { content: "Hello 2", priority: "Mid", hour: "8" },
+      { content: "Hello 3", priority: "Low", hour: "9" },
+      { content: "Hello 4", priority: "High", hour: "5" }
+    ]
   },
   mutations: {
+    ADD_ITEM: (state, item) => {
+      state.PBitems.push(item)
+    },
+    REMOVE_ITEM: (state, item) => {
+      state.PBitems.splice(item, 1)
+    }
   },
   actions: {
+    addPB: ({ commit }, item) => {
+      commit('ADD_ITEM', item)
+    },
+    removePB: ({ commit }, item) => {
+      commit('REMOVE_ITEM', item)
+    }
   },
   modules: {
   }
